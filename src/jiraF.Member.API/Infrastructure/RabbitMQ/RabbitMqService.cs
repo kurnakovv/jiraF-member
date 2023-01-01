8e91d2f6c0d4bdc3deecabc72089bb566deecc0e
KurnakovMaksim <maksk210@gmail.com>
2023-01-01 16:31:28 +0900

    #38 Add production RabbitMQ HostName

--- src/jiraF.Member.API/Infrastructure/RabbitMQ/RabbitMqService.cs
+++ src/jiraF.Member.API/Infrastructure/RabbitMQ/RabbitMqService.cs
@@ -6,6 +6,14 @@ namespace jiraF.Member.API.Infrastructure.RabbitMQ;
 
 public class RabbitMqService : IRabbitMqService
 {
+    private readonly IConfiguration _configuration;
+
+    public RabbitMqService(
+        IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
 	public void SendMessage(object obj)
 	{
 		var message = JsonSerializer.Serialize(obj);
@@ -14,7 +22,10 @@ public class RabbitMqService : IRabbitMqService
 
 	public void SendMessage(string message)
 	{
-        ConnectionFactory factory = new() { HostName = "localhost" };
+        ConnectionFactory factory = new()
+        {
+            HostName = _configuration.GetValue<string>("RabbitMQ:HostName"),
+        };
         using IConnection connection = factory.CreateConnection();
         using IModel channel = connection.CreateModel();
         channel.QueueDeclare(queue: "MemberQueue",
