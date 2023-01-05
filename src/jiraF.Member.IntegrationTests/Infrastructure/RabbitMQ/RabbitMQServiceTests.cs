7a3bd0d748bae638ff3526e37cdabbead14e01bd
KurnakovMaksim <maksk210@gmail.com>
2023-01-05 19:44:18 +0900

    #42 Add production test for SendMessage(string)

---
+++ src/jiraF.Member.IntegrationTests/Infrastructure/RabbitMQ/RabbitMQServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using jiraF.Member.API.Infrastructure.RabbitMQ;
+using Microsoft.Extensions.Configuration;
+
+namespace jiraF.Member.IntegrationTests.Infrastructure.RabbitMQ
+{
+    public class RabbitMQServiceTests
+    {
+        private readonly IConfiguration _configuration;
+        private readonly RabbitMqService _rabbitMqService;
+
+        public RabbitMQServiceTests()
+        {
+            Dictionary<string, string> inMemorySettings = new() {
+                {"RABBITMQ_DEFAULT_USER", Environment.GetEnvironmentVariable("RABBITMQ_DEFAULT_USER")},
+                {"RABBITMQ_DEFAULT_PASS", Environment.GetEnvironmentVariable("RABBITMQ_DEFAULT_PASS")},
+                {"RabbitMQ:HostName", "armadillo-01.rmq.cloudamqp.com"},
+            };
+            _configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(inMemorySettings)
+                .Build();
+            _rabbitMqService = new RabbitMqService(_configuration);
+        }
+
+        [Fact]
+        public void SendMessage_CanSendMessageWithStringWithoutException_SuccessWithoutExceptions()
+        {
+            Exception exception = Record.Exception(() => _rabbitMqService.SendMessage("TestMessage"));
+            Assert.Null(exception);
+        }
+    }
+}
